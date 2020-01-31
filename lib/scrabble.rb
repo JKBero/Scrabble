4e2b2d5bb6e0407440df6226d613de638658282e
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-31 14:24:18 +0000

    feat: word 'at' is worth 2 points

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,9 +1,11 @@
 class Scrabble
 
   def initialize(word)
+    @word = word
   end
 
   def score
+    return 2 if @word == 'at'
     1
   end
 
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,6 +7,13 @@ describe Scrabble do
     it 'returns 1 point when given a character worth 1 point' do
       scrabble = Scrabble.new('a')
       expect(scrabble.score).to eq 1
+      scrabble = Scrabble.new('e')
+      expect(scrabble.score).to eq 1
+    end
+
+    it 'returns 2 points when given a word with two 1 point characters' do
+      scrabble = Scrabble.new('at')
+      expect(scrabble.score).to eq 2
     end
 
   end
